b9d39bed49f9097754516cf975cfdb0f62b973c0
Alexander Stolar <alexander.stolar@yahoo.com>
2018-07-08 22:28:18 +0200

    checking git config

--- ZalandoFastSearchSwift/ZalandoFastSearchSwift/AppDelegate.swift
+++ ZalandoFastSearchSwift/ZalandoFastSearchSwift/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+    
+        window = UIWindow(frame: .zero)
+        window?.rootViewController = UINavigationController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
